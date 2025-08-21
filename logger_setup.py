59a52bfd0cf34af7ce90691e6050399dca9b75f4
David Luke <david.luke@mailbox.org>
2025-08-21 21:39:20 +0200

    feature: new module for logger setup to make

---
+++ logger_setup.py
@@ -0,0 +1,47 @@
+import logging
+from pathlib import Path
+
+
+home_directory = Path.home()  # home dir of user; platform independent
+log_directory = home_directory / ".util_log"  # log directory in home
+home_log_path = log_directory / "file_org.log"  # Define log file path
+
+"""
+Inherits from logging.Handler and overrides emit method.
+Unused  at the moment but will be used later.
+"""
+
+
+class SeparatorLine(logging.Handler):
+    def __init__(self, log_file):
+        super().__init__()
+        self.log_file = (
+            log_file  # Store log file path; Got this part from stackOverflow
+        )
+
+    def emit(self, record):
+        with open(self.log_file, "a") as f:
+            f.write("\n" + "-" * 100 + "\n")
+
+
+def setup_logging():
+    """
+    Set up logging configuration. Make it reusable for other scripts.
+    This function creates a log directory in the user's home directory
+    and sets up a log file to record events.
+    """
+
+    if not log_directory.exists():  # Check if log directory exists
+        log_directory.mkdir(parents=True, exist_ok=True)  # Create it if not
+
+    # Set up basic logging configuration
+    logging.basicConfig(
+        level=logging.INFO,
+        filename=str(home_log_path),
+        # Include date and time
+        format="%(asctime)s - %(levelname)s - %(message)s",
+        datefmt="%Y-%m-%d_%H-%M-%S",  # Specify the date and time format
+    )
+
+    with open(home_log_path, "a") as f:
+        f.write("\n" + "-" * 100 + "\n")
